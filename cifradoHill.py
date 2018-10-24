c3109f4dbf39fb965b1763ba1a4503462a0a00c4
Antonio <reytono@gmail.com>
2018-10-23 18:55:07 -0600

    comentarios

--- cifradoHill.py
+++ cifradoHill.py
@@ -1,15 +1,19 @@
 '''
 Antonio Reyes
-Cifrado de Hill con matrices 2x2
+Cifrado de Hill con matrices 2xn
 '''
 import math
 import numpy
 
+
+#Diccionario para convertir el valor ingresado a numeros
 def abc(diccionario):
     return {' ':0, 'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 
     'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26, 
     '!':27, '?':28}
 
+
+#Diccionario para convertir los numeros a letras para descifrar
 def cba(diccionario2):
     return {'0':" ", '1':"A", '2':"B", '3':"C", '4':"D", '5':"E", '6':"F", '7':"G", '8':"H", '9':"I", '10':"J",
     '11':"K", '12':"L", '13':"M", '14':"N", '15':"O", '16':"P", '17':"Q", '18':"R", '19':"S", '20':"T", '21':"U", '22':"V", '23':"W",
@@ -29,19 +33,23 @@ def cifrar():
     cifrado = []
     final = []
 
-
+    #Ingresa Matriz y llave
     texto = raw_input("Ingrese el texto a cifrar: ")
     llave = raw_input("Ingrese 4 numeros para tener la llave: ")
 
+    #convertir len de texto en par agregando un espacio si es necesario.
     if len(texto)%2 != 0:
         texto = texto + " "
 
+    #crear matriz para llave
     for i in range(0,len(matrizllave)):
         matrizllave[i] = int(llave[i])
 
+    #crear matriz para texto. las letras son numeros
     for i in range(0,len(texto)):
         matriztexto.append(diccionario[texto[i]])
 
+    #multiplicacion de matriz de llave y texto
     x = 0
     y = 1
     while y<len(texto):
@@ -60,8 +68,7 @@ def cifrar():
     for i in range(0,len(cifrado)):
         final.append(diccionario2[str(cifrado[i])])
 
-    #print cifrado
-
+    #imprimir cifrado
     print final
     
 
@@ -147,7 +154,8 @@ def main():
             cifrar()
     
         if opcion == 2:
-            solucionar()
+            print "solucionar"
+            #solucionar()
 
 
 main()
